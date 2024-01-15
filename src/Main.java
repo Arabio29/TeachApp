c5ec00a94d7fd948a2610fb6eecc162df5e7fab4
eliasib <ecantor.2906@gmail.com>
2024-01-14 21:54:48 -0500

    objetoCelular

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ public class Main {
         // Crear instancias de la clase Producto
         Producto producto1 = new Producto("Producto A", 20.0, 10);
         Producto producto2 = new Producto("Producto B", 15.0, 5);
+        Producto celular = new Producto("Tecno pova 5", 650, 38);
 
         // Simular una venta exitosa
         producto1.realizarVenta(3);
 
         // Intentar vender más unidades de las disponibles
         producto2.realizarVenta(7);
+
+        celular.realizarVenta(32);
 
     }
 }
